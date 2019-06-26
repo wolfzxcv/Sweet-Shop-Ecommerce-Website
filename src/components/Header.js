3fa1a31bb3b476464413af06f00845cf224c978b
Nien <wolfzxcv@gmail.com>
2019-06-26 15:57:33 +0200

    nav res done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,65 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { useMedia } from 'use-media';
+import HeaderLaptop from './HeaderLaptop'
+import HeaderMobile from './HeaderMobile'
 
-const Header = ({ className }) => {
+const Header = () => {
+  const isLaptop = useMedia({ minWidth: 769 });
+
   return (
-    <header className={className}>
-      <div className='logo'>
-        <div className='logo-dark'><img src={require('../svg/logo-dark.svg')} alt="logo" /></div>
-        <div className='logotype-lg-dark'><img src={require('../svg/logotype-lg-dark.svg')} alt="logo" /></div>
-      </div>
-
-      <div className='link'>
-        <div><Link to='/'>HOME</Link></div>
-        <div><Link to='/product'>PRODUCT</Link></div>
-        <div><Link to='/login'>LOGIN</Link></div>
-        <div><Link to='/cart'><FontAwesomeIcon icon={faShoppingCart} /></Link></div>
-      </div>
-
-    </header>
+    <>
+      {isLaptop ? <HeaderLaptop /> : <HeaderMobile />}
+    </>
   )
 }
 
-Header.propTypes = {
-  className: PropTypes.string,
-};
-
-const StyledHeader = styled(Header)`
-height: 100px;
-padding: 0px 42px;
-display: flex;
-justify-content: space-between;
-align-items: center;
-
-.logo{
-  display: flex;
-  align-items: center;
-  .logo-dark{
-    margin-right: 8px;
-    width:40px;
-    height:40px;
-  }
-  .logotype-lg-dark{
-    width: 171px;
-    height: 26px;
-  }
-}
-
-.link{
-  display: flex;
-  div{
-    margin-left: 60px;
-    height: 40px;
-    display: flex;
-    align-items: center;
-  }
-}
-`;
-
-StyledHeader.displayName = 'Header';
-
-export default StyledHeader;
+export default Header;
