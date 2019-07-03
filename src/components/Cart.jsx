27cc59ea314a9849c3ffb3b9c31d0c7244dbc14d
Nien <wolfzxcv@gmail.com>
2019-07-03 13:11:43 +0100

    fake cart+rwd

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,16 +1,148 @@
 import React from 'react';
+import { useMedia } from 'use-media';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import CartExample from './CartExample';
+import CartExampleM from './CartExampleM';
+import Underline from './Underline';
 
 const Cart = ({ className }) => {
-  return <div className={className}>Cart</div>;
+  const isLaptop = useMedia({ minWidth: 769 });
+  return (
+    <div className={className}>
+      <div className='left'>
+        <div className='yourcart'>Your cart</div>
+        {isLaptop ? <CartExample /> : <CartExampleM />}
+        {isLaptop && <Underline />}
+        {isLaptop ? <CartExample /> : <CartExampleM />}
+        {isLaptop && <Underline />}
+        {isLaptop ? <CartExample /> : <CartExampleM />}
+      </div>
+
+      <div className='right'>
+        <div className='summary'>Summary</div>
+        <div>
+          <div className='price'>
+            <div>price</div>
+            <div>€ 26.49</div>
+          </div>
+          <div className='shipment'>
+            <div>shipment</div>
+            <div>€ 15.00</div>
+          </div>
+          <div className='total'>
+            <div>Total</div>
+            <div>€ 41.49</div>
+          </div>
+        </div>
+
+        <div className='checkout'>Proceed to checkout</div>
+      </div>
+    </div>
+  );
 };
 
 Cart.propTypes = {
   className: PropTypes.string.isRequired,
 };
 
-const StyledCart = styled(Cart)``;
+const StyledCart = styled(Cart)`
+  display: flex;
+  justify-content: center;
+
+  @media (min-width: 769px) {
+    margin: 50px 0;
+    .yourcart {
+      width: 92%;
+    }
+    .right {
+      color: ${props => props.theme.colors.greenWhite};
+      background-color: ${props => props.theme.colors.green};
+      .summary {
+        border-bottom: 1px solid ${props => props.theme.colors.greenWhite};
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .left {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .yourcart {
+        width: 95vw;
+        margin-bottom: 30px;
+      }
+      display: flex;
+      justify-content: center;
+    }
+    .right {
+      color: ${props => props.theme.colors.green};
+      background-color: ${props => props.theme.colors.white};
+      .summary {
+        margin-top: 20px;
+        background-color: ${props => props.theme.colors.greenWhite};
+      }
+    }
+  }
+
+  .yourcart {
+    height: 65px;
+    font-size: 24px;
+    background-color: ${props => props.theme.colors.greenWhite};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .right {
+    width: 300px;
+    height: 293px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+
+    .summary {
+      width: 100%;
+      height: 50px;
+      font-size: 24px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .price,
+    .shipment,
+    .total {
+      width: 280px;
+      display: flex;
+      justify-content: space-between;
+      line-height: 2.5;
+    }
+    .total {
+      font-size: 20px;
+    }
+
+    .checkout {
+      width: 100%;
+      height: 50px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-bottom: 3px solid ${props => props.theme.colors.orange};
+      font-size: 24px;
+      color: ${props => props.theme.colors.green};
+      background-color: ${props => props.theme.colors.orange};
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
+`;
 
 StyledCart.displayName = 'Cart';
 
---
+++ src/components/CartExampleM.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const CartExampleM = ({ className }) => {
+  return (
+    <div className={className}>
+      <div className='upper'>
+        <div className='left'>
+          <img
+            src={require('../images/photo-1499635842761-4f1f28fafcff.jpg')}
+            alt=''
+          />
+        </div>
+
+        <div className='right'>
+          <div className='items'>
+            <div>Best Sweet</div>
+            <div className='price2'>€ 5.99</div>
+          </div>
+          <div className='amount'>
+            <div>-</div>
+            <input type='text' />
+            <div>+</div>
+          </div>
+        </div>
+      </div>
+      <div className='price'>€ 6.20</div>
+    </div>
+  );
+};
+
+CartExampleM.propTypes = {
+  className: PropTypes.string.isRequired,
+};
+
+const StyledCartExampleM = styled(CartExampleM)`
+  width: 270px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  font-size: 20px;
+  /* div {
+    border: 1px solid red;
+  } */
+
+  img {
+    width: 110px;
+    height: 110px;
+  }
+
+  .upper {
+    margin: 10px 0;
+    display: flex;
+
+    .right {
+      height: 110px;
+      width: 260px;
+      display: flex;
+      flex-direction: column;
+      .items {
+        width: 150px;
+        line-height: 1.5;
+        .price2 {
+          font-size: 16px;
+        }
+      }
+      .amount {
+        width: 150px;
+        display: flex;
+        justify-content: space-evenly;
+        div {
+          width: 48px;
+          height: 48px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border: 1px solid ${props => props.theme.colors.greenWhite};
+        }
+        input {
+          padding: 0;
+          width: 48px;
+          height: 46px;
+        }
+      }
+    }
+  }
+
+  .price {
+    padding: 15px 0;
+    border-top: 1px solid ${props => props.theme.colors.greenWhite};
+    border-bottom: 1px solid ${props => props.theme.colors.greenWhite};
+    font-weight: 700;
+    width: 320px;
+    display: flex;
+    justify-content: flex-end;
+  }
+`;
+
+StyledCartExampleM.displayName = 'CartExampleM ';
+
+export default StyledCartExampleM;
