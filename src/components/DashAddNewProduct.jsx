4f05a6b0fd0578e3db9f61ad7398bcf208cc57d4
Nien <wolfzxcv@gmail.com>
2019-07-07 13:05:14 +0100

    basic static add new porduct form done

--- src/components/DashAddNewProduct.jsx
+++ src/components/DashAddNewProduct.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 import Modal from 'styled-react-modal';
+import styled from 'styled-components';
+import { Button, Flex } from 'rebass';
 import { SharedContext } from '../contexts/SharedContext';
 
 const DashAddNewProduct = () => {
@@ -7,25 +9,134 @@ const DashAddNewProduct = () => {
   return (
     <StyledModal
       isOpen={isModalOpen}
-      onBackgroundClick={() => setIsModalOpen(!isModalOpen)}
-      onEscapeKeydown={() => setIsModalOpen(!isModalOpen)}
+      onEscapeKeydown={() => setIsModalOpen(false)}
     >
-      <span>Norge elsker meg!!!!</span>
-      <button type='button' onClick={() => setIsModalOpen(!isModalOpen)}>
-        Close me and move to Norway
-      </button>
+      <StyledTitle>Add New Product || Norge elsker meg</StyledTitle>
+
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          alert('Love you');
+        }}
+      >
+        <Flex m={2} justifyContent='space-around'>
+          <StyledFlex flexDirection='column' justifyContent='flex-start'>
+            <div>Input image url</div>
+            <input type='text' placeholder='Input image url' />
+            <div>or Upload image</div>
+            <input type='file' />
+          </StyledFlex>
+          <Flex flexDirection='column' justifyContent='space-around'>
+            <div>
+              Title
+              <input
+                type='text'
+                placeholder='write your product name'
+                size='50'
+              />
+            </div>
+
+            <div>
+              Category
+              <input type='text' placeholder='write your category' size='20' />
+            </div>
+
+            <div>
+              Enabled
+              <input type='checkbox' />
+            </div>
+
+            <div>
+              Price
+              <input
+                type='text'
+                placeholder='€99.99'
+                maxLength='5'
+                size='5'
+                style={{ direction: 'rtl' }}
+              />
+            </div>
+
+            <div>
+              Amount
+              <input
+                type='text'
+                placeholder='999'
+                maxLength='3'
+                size='3'
+                style={{ direction: 'rtl' }}
+              />
+            </div>
+
+            <div>
+              <div>Allergy Advice</div>
+              <textarea placeholder='Allergy Advice...' rows='3' cols='60' />
+            </div>
+
+            <div>
+              <div>Details</div>
+              <textarea placeholder='Details...' rows='10' cols='60' />
+            </div>
+          </Flex>
+        </Flex>
+      </form>
+
+      <Flex justifyContent='flex-end'>
+        <StyledButton
+          m={1}
+          width='150px'
+          bg='green'
+          onClick={() => setIsModalOpen(false)}
+          onKeyDown={e => {
+            if (e.keyCode === 27) setIsModalOpen(false);
+          }}
+        >
+          Close
+        </StyledButton>
+        <StyledButton
+          m={1}
+          ml='30px'
+          width='150px'
+          bg='green'
+          onClick={() => {
+            alert('Love you');
+            setIsModalOpen(false);
+          }}
+        >
+          Submit
+        </StyledButton>
+      </Flex>
     </StyledModal>
   );
 };
 
 const StyledModal = Modal.styled`
-  width: 20rem;
-  height: 20rem;
+  width: 800px;
+  height: 600px;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: space-around;
+  justify-content: space-between;
+  border: 2px solid ${props => props.theme.colors.green};
   background-color: ${props => props.theme.colors.white};
+`;
+
+const StyledTitle = styled.div`
+  padding-left: 8px;
+  color: ${props => props.theme.colors.white};
+  background-color: ${props => props.theme.colors.green};
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 2;
+`;
+
+const StyledFlex = styled(Flex)`
+  height: 500px;
+`;
+
+const StyledButton = styled(Button)`
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 export default DashAddNewProduct;
--- src/components/DashContent.jsx
+++ src/components/DashContent.jsx
@@ -3,15 +3,13 @@ import axios from 'axios';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { ModalProvider } from 'styled-react-modal';
-//import { Flex, Box, Text } from 'rebass';
+import { Button } from 'rebass';
 import DashProductList from './DashProductList';
 import DashAddNewProduct from './DashAddNewProduct';
 import { SharedContext } from '../contexts/SharedContext';
 
 const DashContent = ({ className }) => {
-  const { isModalOpen, setIsModalOpen, product, setProduct } = useContext(
-    SharedContext
-  );
+  const { setIsModalOpen, product, setProduct } = useContext(SharedContext);
 
   useEffect(() => {
     axios
@@ -26,9 +24,18 @@ const DashContent = ({ className }) => {
   return (
     <ModalProvider>
       <div className={className}>
-        <button type='button' onClick={() => setIsModalOpen(!isModalOpen)}>
+        <Button
+          className='add-new-porduct'
+          type='button'
+          width='100%'
+          m={1}
+          mx='auto'
+          color='green'
+          bg='greenWhite'
+          onClick={() => setIsModalOpen(true)}
+        >
           Add new porduct
-        </button>
+        </Button>
         <div className='table-head'>
           <div className='wide'>Category</div>
           <div className='wider'>Name</div>
@@ -62,8 +69,16 @@ DashContent.propTypes = {
 };
 
 const StyledDashContent = styled(DashContent)`
+  margin: 0 auto;
   div {
     border: 1px solid red;
+  }
+  .add-new-porduct {
+    &:hover {
+      font-weight: 700;
+      border: 1px solid ${props => props.theme.colors.green};
+      cursor: pointer;
+    }
   }
   .table-head {
     display: flex;
